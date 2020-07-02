a79d5f7b5f361257d14d887c9809897a5891ac11
Yujin Lee <yujin092444@gmail.com>
2020-07-02 18:29:19 +0900

    초기 레이아웃 구현

---
+++ app/src/main/java/com/example/myapplication/CheckActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.myapplication.R;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.Spinner);
+        String[] str = getResources().getStringArray(R.array.location);
+
+        final ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, R.layout.spinner_item, str);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
