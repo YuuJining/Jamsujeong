6cef0d5101ce8e1102eed75ac01320019ccaecc2
hyu0076 <hyu0076@gmail.com>
2020-07-05 02:12:06 +0900

    좌석 배정 시간 10분 단위로 2시간 까지 설정할 수 있는 Numberpicker 구현

--- app/src/main/java/com/example/myapplication/AssignActivity.java
+++ app/src/main/java/com/example/myapplication/AssignActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,17 @@ public class AssignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign);
+
+        NumberPicker hpicker = (NumberPicker) findViewById(R.id.hpicker);
+        hpicker.setMinValue(0);
+        hpicker.setMaxValue(2);
+        hpicker.setWrapSelectorWheel(false);
+
+        NumberPicker mpicker = (NumberPicker) findViewById(R.id.mpicker);
+        mpicker.setMinValue(0);
+        mpicker.setMaxValue(6);
+        mpicker.setDisplayedValues(new String[]{"0", "10", "20", "30", "40", "50", "60"});
+        mpicker.setWrapSelectorWheel(false);
 
         Button positive = (Button) findViewById(R.id.positiveButton);
         positive.setOnClickListener(new View.OnClickListener() {
