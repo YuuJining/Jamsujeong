83d4e77d91ababbd4d4f62c97a79efa65517632a
hyu0076 <hyu0076@gmail.com>
2020-08-18 17:19:15 +0900

    hpicker == 2일때 mpicker 0으로 바꾼 후 조작 불가 설정

--- app/src/main/java/com/example/myapplication/SetTimeActivity.java
+++ app/src/main/java/com/example/myapplication/SetTimeActivity.java
@@ -20,6 +20,9 @@ public class SetTimeActivity extends AppCompatActivity {
 
     Context context;
     CountDownTimer countDownTimer;
+    NumberPicker hpicker;
+    NumberPicker mpicker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +30,23 @@ public class SetTimeActivity extends AppCompatActivity {
 
         context = this;
 
-        final NumberPicker hpicker = (NumberPicker) findViewById(R.id.hpicker);
+        hpicker = (NumberPicker) findViewById(R.id.hpicker);
         hpicker.setMinValue(0);
         hpicker.setMaxValue(2);
-        hpicker.setWrapSelectorWheel(false);
-
-        final NumberPicker mpicker = (NumberPicker) findViewById(R.id.mpicker);
+        mpicker = (NumberPicker) findViewById(R.id.mpicker);
         mpicker.setMinValue(0);
         mpicker.setMaxValue(59);
 
-        if(hpicker.getValue() == 2) {
-            mpicker.setDisplayedValues(new String[] {"0"});
-        }
+        hpicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if(newVal == 2){
+                    mpicker.setValue(0);
+                    mpicker.setEnabled(false);
+                } else mpicker.setEnabled(true);
+            }
+        });
+
         mpicker.setDisplayedValues(new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                                                 "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
                                                 "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
